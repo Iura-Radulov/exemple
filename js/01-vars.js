7c2c90d575b85c1396b6f128f00edf89bf8a980d
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-01-19 23:00:19 +0200

    Update 01-vars.js

--- js/01-vars.js
+++ js/01-vars.js
@@ -6,3 +6,33 @@
 // const type = typeof 'word'
 // console.log(type)
 
+
+// ЦИКЛ   FOR 
+
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employees = 10
+// let totalSalary = 0
+// for (let i = 1; i <= employees; i += 1) {
+//     const salary = Math.round(
+//         Math.random() * (maxSalary - minSalary) + minSalary,
+//     )
+//     console.log(`ЗП работника номер ${i} - ${salary}`);
+//     totalSalary += salary
+// }
+// console.log(`totalSalary: `, totalSalary)
+
+
+// const min = 5
+// const max = 13
+// let total = 0
+// for (let i = min; i <= max; i += 1) {
+    
+//     if (i % 2 !== 0) {
+//         console.log(`не четное: `, i)
+//         continue;
+//     }
+//     console.log(`четное:`, i)
+//         total += i;
+// }
+// console.log(`total: `, total)
