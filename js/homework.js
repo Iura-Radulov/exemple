113b6b2f41d9d0c77d6389f9a6fbf93c10cbb2e2
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-01-31 20:48:54 +0200

    JS-Modul-1,2

---
+++ js/homework.js
@@ -0,0 +1,77 @@
+// function slugify(title) {
+//   // Change code below this line
+// // const string = title.split(" ").toLowerCase;
+// // const slug = string.join("-");
+//     // console.log(title.split(" ").join("-").toLowerCase)
+//     return title.split(" ").join("-").toLowerCase();
+
+//   // Change code above this line
+// }
+// slugify("Arrays for begginers")
+// console.log(slugify("Arrays for begginers"))
+
+
+// function findLongestWord(string) {
+//   // Change code below this line
+// const transferWord = string.split(" ");
+
+//     let longestWord = transferWord[0];
+//     for (const word of transferWord) {
+
+//         if (word.length > longestWord.length) {
+//   longestWord = word;
+// }
+//     }
+
+// return longestWord;
+
+//   // Change code above this line
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+
+
+// function filterArray(numbers, value) {
+//    // Change code below this line
+// let bigNumbers = [];
+// for (const number of numbers) {
+//   if (number > value) {
+//     bigNumbers.push(number);
+//   }
+// }
+// return bigNumbers
+
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3))
+
+// function includes(array, value) {
+//   // Change code below this line
+//     for (i = 0; i <= array.length; i += 1) {
+//     // console.log(array[i])
+//     if (array[i] === value) {
+      
+//     return true;
+//     }
+        
+   
+//     }
+//     return false;
+//   // Change code above this line
+// }
+
+// console.log(includes([1, 2, 3, 4, 5], 3))
+
+// console.log(includes([1, 2, 3, 4, 5], 17))
+
+
+
+
+
+
+
+
+
+
+
